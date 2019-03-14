ee4f1d1106f2c27ba6c7f6fa852b4fdd63d42d1f
stef <stephane.bachelier@gmail.com>
2019-03-14 15:11:41 +0100

    feat: add support for injecting stylesheet

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const tasks = require('./lib/tasks')
 
-const run = async baseDir => {
+const run = async ({ baseDir, css }) => {
   try {
     const resolver = await tasks.tmpDir({ baseDir })
 
     await tasks.minifyCss(resolver)
 
-    await tasks.inlineCss(resolver)
+    await tasks.inlineCss(resolver, { css })
 
     await tasks.copy(resolver)
   } catch (e) {
